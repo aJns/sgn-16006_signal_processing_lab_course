2743c9290c7332302b8d807cce980f155d5a1c36
Vili Saura <saura@student.tut.fi>
2017-03-03 14:48:13 +0200

    Added comments

--- lab1/work/bilinear_interpolation.m
+++ lab1/work/bilinear_interpolation.m
@@ -2,16 +2,27 @@ function processedImage = bilinear_interpolation(rawImage)
 R = rawImage(:, :, 1);
 G = rawImage(:, :, 2);
 B = rawImage(:, :, 3);
+
+%Added zeros to fill the "missing" pixels
 Rpad = padarray(R, [1 1]);
 Gpad = padarray(G, [1 1]);
 Bpad = padarray(B, [1 1]);
 fullImage = zeros([size(R) 3]);
 
+%Notice that padding adds 2 rows and 2 columns,
+%so Gpad(2,2) corresponds to G(1,1)
+%Same thing happens with all color channels
+
+%Green computations
+%Both even and odd rows compute the green value the same way, but their column
+%indexes start from different points
 for i = 2:size(Gpad,1)-1
+    %Even rows
     if(mod(i,2)== 0)
     for a = 2:2:size(Gpad,2)-1
         G(i-1,a-1) = (Gpad(i,a+1)+Gpad(i,a-1)+ Gpad(i-1,a)+Gpad(i+1,a))/4;
     end
+    %Odd rows
     else
      for b = 3:2:size(Gpad,2)-1
         G(i-1,b-1) = (Gpad(i,b+1)+Gpad(i,b-1)+ Gpad(i-1,b)+Gpad(i+1,b))/4;
@@ -19,11 +30,15 @@ for i = 2:size(Gpad,1)-1
     end
 end
 
+%Blue computations
 for i = 2:size(Bpad,1)-1
+    %Even rows compute the value from 2 other values
     if(mod(i,2)== 0)
         for a = 3:2:size(Bpad,2)-1
             B(i-1,a-1) = (Bpad(i,a+1)+Bpad(i,a-1))/2;
         end
+    %Odd rows compute the value from 2 or 4 other values, depending on the
+    %column index
     else
          for b = 2:2:size(Bpad,2)-1
              B(i-1,b-1) = (Bpad(i+1,b)+Bpad(i-1,b))/2;
@@ -34,6 +49,10 @@ for i = 2:size(Bpad,1)-1
     end
 end
 
+%Red computations
+%Similar to blue computations, but the rows are exchanged, i.e. Even rows
+%use different functions depending on the column index and odd rows compute
+%the value from 2 other values
 for i = 2:size(Rpad,1)-1
     if(mod(i,2)== 0)
         for a = 2:2:size(Rpad,2)-1
