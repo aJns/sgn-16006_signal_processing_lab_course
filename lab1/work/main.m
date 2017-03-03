4f7e1b827c957949959affc72b6570e3489945dc
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-03-03 14:27:39 +0200

    Tweak main.m a bit further

--- lab1/work/main.m
+++ lab1/work/main.m
@@ -67,17 +67,19 @@ cornerT = 200;
 areaSize = 100;
 
 figure;
-subplot(3, 3, 1); imshow(nnImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
-subplot(3, 3, 2); imshow(blImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
-subplot(3, 3, 3); imshow(ppgImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
+h(1) = subplot(3, 3, 1); imshow(nnImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
+h(2) = subplot(3, 3, 2); imshow(blImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
+h(3) = subplot(3, 3, 3); imshow(ppgImage2(corner2:corner2+areaSize, corner2:corner2+areaSize, :));
 
-subplot(3, 3, 4); imshow(nnImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
-subplot(3, 3, 5); imshow(blImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
-subplot(3, 3, 6); imshow(ppgImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
+h(4) = subplot(3, 3, 4); imshow(nnImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
+h(5) = subplot(3, 3, 5); imshow(blImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
+h(6) = subplot(3, 3, 6); imshow(ppgImage5(corner5:corner5+areaSize, corner5:corner5+areaSize, :));
 
-subplot(3, 3, 7); imshow(nnTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
-subplot(3, 3, 8); imshow(blTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
-subplot(3, 3, 9); imshow(ppgTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
+h(7) = subplot(3, 3, 7); imshow(nnTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
+h(8) = subplot(3, 3, 8); imshow(blTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
+h(9) = subplot(3, 3, 9); imshow(ppgTestImage(cornerT:cornerT+areaSize, cornerT:cornerT+areaSize, :));
+
+linkaxes(h, 'xy');
 
 
 methods = { 'NN-interpolation', 'Bilinear interpolation', 'PPG interpolation' };
