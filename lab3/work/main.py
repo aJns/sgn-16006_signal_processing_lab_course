f8adcd25e93e6a0c82e8db13f2099ba56c75c620
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-04-02 20:52:51 +0300

    Finish assignment 1

--- lab3/work/main.py
+++ lab3/work/main.py
@@ -37,9 +37,42 @@ if __name__ == "__main__":
         if frameEnd >= dataLength: break
         frameDiff = frameEnd-frameStart
         frameArray[i,:] = data[frameStart:frameEnd]*hann
+
+    bins = 1024
+    kHzFreqBands = np.array([
+        [0,   0.5],
+        [0.5, 1],
+        [1,   2],
+        [2,   4]
+        ])
+
+    fftFrequencies = np.fft.fftfreq(bins, 1/Fs)
+
+    frame50DFT = np.fft.fft(frameArray[50,:], bins)
     
+    plt.figure()
     plt.plot(frameArray[50,:])
 
+    plt.figure()
+    plt.plot(fftFrequencies, np.abs(frame50DFT))
+
+    totalEnergy = np.sum(np.power(np.abs(frame50DFT[0:np.int(bins/2)]),2))
+
+    binFreqRatio = (bins/Fs)
+
+    binIndices = (kHzFreqBands*(binFreqRatio*1000)).astype(int)
+
+    bandCount = 4
+    bandEnergyRatios = np.zeros(bandCount)
+
+    for i in range(bandCount):
+        start = binIndices[i, 0]
+        end = binIndices[i, 1]
+        bandEnergy = np.sum(np.power(np.abs(frame50DFT[start:end]), 2))
+        bandEnergyRatios[i] = bandEnergy/totalEnergy
+
+    plt.figure()
+    plt.bar(range(4), bandEnergyRatios, tick_label=["0 - 0.5 kHz", "0.5 - 1 kHz", "1 - 2 kHz", "2 - 4 kHz"])
 
 
 
